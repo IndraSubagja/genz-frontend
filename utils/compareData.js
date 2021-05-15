04ce3cb83b715445b5718d29dcd12cc22ec1b052
Indra Subagja <indrasubagja0000@gmail.com>
2021-05-15 22:53:02 +0700

    remove

--- utils/compareData.js
+++
@@ -1,9 +0,0 @@
-const simplifyCartData = (cart) =>
-  JSON.stringify(
-    cart?.map((item) => ({
-      qty: item.qty,
-      product: item.product.id,
-    }))
-  );
-
-export const compareCartData = (x, y) => simplifyCartData(x) === simplifyCartData(y);
--- utils/sortImages.js
+++
@@ -1,3 +0,0 @@
-export default function sortImages(product) {
-  product.images.sort((a, b) => a.name.localeCompare(b.name));
-}
